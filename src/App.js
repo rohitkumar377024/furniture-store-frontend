b157f0001a0d0f467f9758e28944638085284434
Rohit <rohitkumar377024@gmail.com>
2020-07-09 06:13:11 +0530

    Initial Commit: Basic Nav Bar Dummy + Landing Page Dummy

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav">
+        <h3 className="nav-brand">Furniture Store</h3>
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>Welcome to</h1>
+        <h1>your dream furniture</h1>
+      </div>
+
+      <div>Shop Now</div>
+
+      <div>Image Slider WIP</div>
+
+      <div>2020 • NEW DELHI - NCR</div>
+    </div>
+  );
+}
+
+export default App;
