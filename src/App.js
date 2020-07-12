f9e9d5be49c7fac1cafac5f8121f77d08716014e
Rohit <rohitkumar377024@gmail.com>
2020-07-12 22:08:55 +0530

    LISTEN: Integrating Bootstrap Now. Cleaned Up for it.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import './App.css';
+import chair from './chair.png';
+import first from './slider-one.jpg';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function App() {
   return (
-    <div className="App">
-      <div className="nav">
-        <h3 className="nav-brand">Furniture Store</h3>
-        <ul className="nav-items">
-          <li>Home</li>
-          <li>Shop</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-        </ul>
-      </div>
-
-      <div>
-        <h1>Welcome to</h1>
-        <h1>your dream furniture</h1>
-      </div>
-
-      <div>Shop Now</div>
-
-      <div>Image Slider WIP</div>
-
-      <div>2020 • NEW DELHI - NCR</div>
+    <div>
+      <h1>Furniture Store</h1>
     </div>
   );
 }
