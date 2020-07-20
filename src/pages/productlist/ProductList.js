678f7440a1a0a023a1dc8052efed07b410949be4
Rohit <rohitkumar377024@gmail.com>
2020-07-20 18:13:57 +0530

    Modifications to Product List and Category Components

--- src/pages/productlist/ProductList.js
+++ src/pages/productlist/ProductList.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import './productlist.scss';
+import categoryImage from '../../assets/category.png';
 
 const ProductList = props => {
   const type = props.match.params.id;
 
   return (
-    <div className="product-list">
-      <h1 className="capitalize">{type}</h1>
+    <div className="container">
+      <div className="row d-flex flex-column">
+        <div className="col">
+          <h1 className="capitalize">{type}</h1>
+        </div>
+        <div className="col d-flex justify-content-center flex-wrap">
+          {/* HERE WE GO */}
+
+          {[...Array(9).keys()].map(num => (
+            <div className="product d-flex flex-column align-items-center no-underline-hover">
+              <img className="img-fluid" src={categoryImage} alt="" />
+              <p className="w-100 product-name">Product Name</p>
+              <p className="w-100 product-price">Product Price</p>
+            </div>
+          ))}
+
+          {/* HERE WE STOP */}
+        </div>
+      </div>
     </div>
   );
 };
